6f2b9c5cb96b686861ef1b3c3eec818058ce613b
Sujin Choi <hissujinc@gmail.com>
2022-09-03 01:33:42 +0900

    Feat: 추가 관리페이지 데이터 fetch
    상품 관리하기 위해 데이터를 가져와야하는 함수 추가 후 랜더링, 그리고 상세 컴포넌트로 props를 넘겨준다

--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -1,3 +1,37 @@
+import { useState, useEffect } from 'react';
+import ManagementProduct from '../components/Management/ManagementProduct';
+
 export default function Management() {
-  return <h1>하이</h1>;
+  const [manageList, setManageList] = useState([]);
+  useEffect(() => {
+    fetch('/data/managementData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setManageList(data);
+      });
+  }, []);
+
+  const productDelete = id => {
+    setManageList(manageList.filter(product => product.id !== id));
+  };
+
+  return (
+    <ul>
+      {manageList.map(product => {
+        return (
+          <ManagementProduct
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            images={product.images}
+            date={product.date}
+            show={product.show}
+            productDelete={productDelete}
+          />
+        );
+      })}
+    </ul>
+  );
 }
