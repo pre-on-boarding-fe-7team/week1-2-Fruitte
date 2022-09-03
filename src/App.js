57bf4e79ac09158de11f566c7e33422311d024f4
Malza0408 <parkjh04080@gmail.com>
2022-09-03 15:34:48 +0900

    Feat: 추가 MAIN Router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { ROUTE } from './common/utils/constant';
+import ProductsList from './components/ProductsList';
 
 function App() {
-  return <h3>hello!</h3>;
+  return (
+    <Routes>
+      <Route path={ROUTE.MAIN} element={<ProductsList />} />
+    </Routes>
+  );
 }
 
 export default App;
