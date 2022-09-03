8a891dd16c618d91cc8928b39c3339ec46bbc586
Sujin Choi <hissujinc@gmail.com>
2022-09-04 01:41:53 +0900

    Design: 변경 헤더 스타일
    링크가 추가 되어 그 관련한 css 추가

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -17,22 +17,33 @@ export const Img = styled.img`
 
 export const Ul = styled.ul`
   display: flex;
+  align-items: center;
   position: absolute;
+  height: 100%;
   left: 50%;
   transform: translateX(-50%);
 `;
+
+export const Li = styled.li`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  list-style: none;
+  padding: 0 12px;
+  color: rgba(0, 0, 0, 0.65);
+
+  a {
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.65);
+  }
+`;
+
 export const UlSign = styled.ul`
   display: flex;
 `;
 
-export const Li = styled.li`
-  list-style: none;
-  padding: 0 12px;
-  color: rgba(0, 0, 0, 0.65);
-`;
-
 export const LiSign = styled.li`
   list-style: none;
-  padding: 0 8px;
-  font-size: 12px;
+  padding: 0 4px;
+  font-size: 14px;
 `;
