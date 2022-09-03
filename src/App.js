ea2d08b109e1bbf636eb8a5affda731acf1a148f
Sujin Choi <hissujinc@gmail.com>
2022-09-04 01:24:31 +0900

    Feat: 추가 헤더, 관리자페이지 위치 변경
    헤더가 없기 때문에 공통 헤더를 추가해줬고, 관리자 페이지의 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Management from './pages/Management';
+import Management from './pages/Management/Management';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        {/* <Route path="/" element={<ProductLists />} /> */}
         <Route path="/manage" element={<Management />} />
       </Routes>
     </BrowserRouter>
