5f3435721aaa1bf3a8cf52f8a50a08a1e65a5075
Malza0408 <parkjh04080@gmail.com>
2022-09-04 13:58:28 +0900

    Refactor: 수정 api get함수
    인수로 endpoint를 넘길 필요가 없기에 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 const baseUrl = String(process.env.REACT_APP_SERVER_URL);
 
-const get = async endpoint => {
-  const url = baseUrl + endpoint;
-  const res = await fetch(url);
+const get = async () => {
+  const res = await fetch(baseUrl);
   if (!res.ok) {
     throw new Error(`${res.status.toString()} Error 인한 요청 실패!`);
   }
