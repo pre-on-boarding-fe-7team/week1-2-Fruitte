b53c59d08e07f656490b375544c94d1f148e2f58
ezn6 <kcourtney@naver.com>
2022-09-05 02:50:56 +0900

    Feat: 추가 getOrderList
    주문 정보를 가져오는 기능 추가

--- src/components/OrderDetail/OrderTable/OrderTable.jsx
+++ src/components/OrderDetail/OrderTable/OrderTable.jsx
@@ -1,38 +1,33 @@
-import React, { useEffect } from 'react';
-import { faker } from '@faker-js/faker';
+import React, { useEffect, useState } from 'react';
+import { get } from '../../../api/api';
 import { Container, Table, Td, SmallSpan, BoldDiv, Button } from './OrderTable.style';
 
 const OrderTable = () => {
-  // useEffect(()=>{
+  const [order, setOrder] = useState([]);
 
-  // })
+  const getOrderList = async () => {
+    const res = await get();
+    setOrder(res.products);
+  };
 
-  const data = Array(7)
-    .fill()
-    .map(() => ({
-      num: faker.random.numeric(5),
-      image: faker.image.cats(70, 70, true),
-      name: faker.name.fullName(),
-      quantity: faker.random.numeric(1),
-      option: faker.random.numeric(1),
-      date: `${new Date().getFullYear()}-${new Date().getMonth() + 1}`,
-      price: faker.commerce.price(1000, 80000, 0),
-    }));
+  useEffect(() => {
+    getOrderList();
+  }, []);
 
   return (
     <Container>
       <Table>
         <tbody>
-          {data.map(({ num, image, name, quantity, option, date, price }) => (
-            <tr key={num}>
+          {order.map(({ id, 주문번호, url, name, salesList, date, price }) => (
+            <tr key={id}>
               <Td>
-                <img src={image} alt="productImage" />
+                <img src={url} alt="productImage" style={{ width: '100px', height: '100px' }} />
               </Td>
               <Td>
                 <BoldDiv>{name}</BoldDiv>
                 <div>
-                  <SmallSpan>수량 : {quantity} | </SmallSpan>
-                  <SmallSpan>옵션 : {option} kg</SmallSpan>
+                  <SmallSpan>수량 : {salesList[0].quantity} | </SmallSpan>
+                  <SmallSpan>옵션 : {salesList[0].title}</SmallSpan>
                 </div>
                 <div>
                   <SmallSpan>{price}원 | </SmallSpan>
@@ -45,7 +40,7 @@ const OrderTable = () => {
                   <SmallSpan>주문번호</SmallSpan>
                 </div>
                 <div>
-                  <SmallSpan>{num}</SmallSpan>
+                  <SmallSpan>{주문번호}</SmallSpan>
                 </div>
                 <Button>리뷰쓰기</Button>
               </Td>
