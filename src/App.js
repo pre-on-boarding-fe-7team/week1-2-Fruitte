b1d3414b63a5bffa9c8f33b2b985c55799e88a94
cssong <a1373052011@gmail.com>
2022-09-04 20:30:48 +0900

    Style: 변경 index, app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Registration from './pages/Registration/Registration';
+import Registration from './components/Registration/Registration';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/regist" element={<Registration />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/regist" element={<Registration />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { GlobalStyle } from './styles/global-styles';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import { GlobalStyle } from './styles/global-styles';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
