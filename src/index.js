80d110809a9b3c997c90dc901da714eba1aa1251
ezn6 <kcourtney@naver.com>
2022-09-04 16:49:05 +0900

    Feat: 추가 styled components initial setting

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global-styles';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
+  // <App />
 );
