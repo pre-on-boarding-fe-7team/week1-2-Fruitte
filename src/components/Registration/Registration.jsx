d5bfc49c2511f6941b065130638070c0ba353206
cssong <a1373052011@gmail.com>
2022-09-05 03:54:27 +0900

    Design: 변경 상품목록 등록 페이지 CSS 수정
    상품목록 등록 페이지의 CSS 수정

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,11 +1,9 @@
-import RegistrationLnb from './Lnb/RegistrationLnb';
 import RegistForm from './RegistForm';
 import { Container } from './Registration.styl';
 
 const Registration = ({ userId }) => {
   return (
     <Container>
-      <RegistrationLnb />
       <RegistForm userId={userId} />
     </Container>
   );
