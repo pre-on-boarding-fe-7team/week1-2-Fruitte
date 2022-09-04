d4720063434ebd3cbaac68e82bc4daf8101043cb
Malza0408 <parkjh04080@gmail.com>
2022-09-04 18:30:48 +0900

    Feat: 추가 상품 price 표시

--- src/components/ProductList.style.js
+++ src/components/ProductList.style.js
@@ -17,9 +17,25 @@ export const Li = styled.li`
     width: 24%;
   }
   width: 48%;
-  margin: 0.2rem;
+  margin: 0.2rem 0.2rem 1rem 0.2rem;
+`;
+
+export const Title = styled.h1`
+  margin: 1rem auto 1rem auto;
 `;
 
 export const Img = styled.img`
   width: 100%;
+  cursor: pointer;
+`;
+
+export const Price = styled.span`
+  font-size: 1.1rem;
+  margin-right: 0.5rem;
+`;
+
+export const SalePrice = styled(Price)`
+  font-size: 1rem;
+  color: gray;
+  text-decoration: line-through;
 `;
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { get } from '../api/api';
 import Pagination from './pagination/Pagination';
-import { Container, Img, Li, Ol } from './ProductList.style';
+import { Container, Img, Li, Ol, Price, SalePrice, Title } from './ProductList.style';
 
 const LIMIT = 10;
 
@@ -19,6 +19,11 @@ function ProductsList() {
   const handleSetPage = page => {
     setPage(Number(page));
   };
+
+  const moneyUnit = price => {
+    return Number(price).toLocaleString('ko-KR');
+  };
+
   useEffect(() => {
     getProductsList();
   }, []);
@@ -28,6 +33,9 @@ function ProductsList() {
         {products?.slice(offest, offest + LIMIT).map((product, index) => (
           <Li key={product.id}>
             <Img src={product.images} alt="이미지입니다." />
+            <Title>{product.name}</Title>
+            <Price>{moneyUnit(product.price)}원</Price>
+            <SalePrice>{moneyUnit(product.salePrice)}원</SalePrice>
           </Li>
         ))}
       </Ol>
@@ -36,7 +44,7 @@ function ProductsList() {
           totalPage={Math.ceil(products.length / LIMIT)}
           page={page}
           handleSetPage={handleSetPage}
-        ></Pagination>
+        />
       </div>
     </Container>
   );
