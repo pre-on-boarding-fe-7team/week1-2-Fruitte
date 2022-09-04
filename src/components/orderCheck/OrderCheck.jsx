9f851eaec5e3f0131da559d8955f6ed1008bef63
ezn6 <kcourtney@naver.com>
2022-09-04 12:40:12 +0900

    Refactor: 수정 테이블
    테이블 구성 요소, 배치 수정

--- src/components/orderCheck/OrderCheck.jsx
+++ src/components/orderCheck/OrderCheck.jsx
@@ -1,45 +1,60 @@
 import React from 'react';
 import { faker } from '@faker-js/faker';
-import { Table } from './OrderCheck.style';
-
-//주문 내역이 없다면 '주문 내역이 없습니다.'
+import { Container, Table, Td } from './OrderCheck.style';
 
 const OrderCheck = () => {
-  const columns = ['주문번호', '    ', '상품명', '수량', '옵션'];
+  // const columns = ['주문번호', '상품명', '수량', '옵션'];
 
-  const data = Array(5)
+  const data = Array(7)
     .fill()
     .map(() => ({
       num: faker.random.numeric(5),
-      image: faker.image.cats(100, 100, true),
+      image: faker.image.cats(70, 70, true),
       name: faker.name.fullName(),
       quantity: faker.random.numeric(1),
       option: faker.random.numeric(1),
+      date: `${new Date().getFullYear()}-${new Date().getMonth() + 1}`,
+      price: faker.commerce.price(1000, 80000, 0),
     }));
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          {columns.map(column => (
-            <th key={column}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(({ num, image, name, quantity, option }) => (
-          <tr key={num}>
-            <td>{num}</td>
-            <td>
-              <img src={image} alt="productImage" />
-            </td>
-            <td>{name}</td>
-            <td>{quantity}</td>
-            <td>{option} kg</td>
+    <Container>
+      <Table>
+        {/* <thead>
+          <tr>
+            {columns.map(column => (
+              <th key={column}>{column}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead> */}
+        <tbody>
+          {data.map(({ num, image, name, quantity, option, date, price }) => (
+            <tr key={num}>
+              <Td>
+                <img src={image} alt="productImage" />
+              </Td>
+              <Td>
+                <div>{name}</div>
+                <div>
+                  <span>수량 : {quantity} | </span>
+                  <span>옵션 : {option} kg</span>
+                </div>
+                <div>
+                  <span>{price}원 | </span>
+                  <span>{date}</span>
+                </div>
+              </Td>
+              <Td>배송중</Td>
+              <Td>
+                <div>주문번호</div>
+                <div>{num}</div>
+                <button>리뷰쓰기</button>
+              </Td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 
