19733af0662056ca454759cc3e4d550747501fc2
ezn6 <kcourtney@naver.com>
2022-09-04 14:59:49 +0900

    Design: OrderDetail CSS

---
+++ src/pages/OrderDetail.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  /* border: 2px solid skyblue; */
+  background-color: rgb(243, 243, 243);
+  padding: 4rem 2rem 0 2rem;
+`;
+
+export const RightSection = styled.div`
+  flex: 1 1 85%;
+  /* border: 2px solid red; */
+  padding: 0 1rem 0 1rem;
+`;
