ed7460d2791b250c77a50f2b84991777c4a378b1
Sujin Choi <hissujinc@gmail.com>
2022-09-05 09:33:13 +0900

    Feat: 추가 상품등록 페이지 버튼
    관리자페이지에서 상품 등록 페이지 이동 버튼이 없어서 추가

--- src/components/Management/Lnb/ManangementLnb.style.js
+++ src/components/Management/Lnb/ManangementLnb.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Ul = styled.ul`
   display: flex;
+  flex-direction: column;
   width: 16%;
   margin: 0;
   padding: 0;
@@ -16,12 +17,37 @@ export const Ul = styled.ul`
 export const Li = styled.li`
   list-style: none;
   margin: 0;
-  padding: 20px 0;
+  padding: 1rem 0;
 
   span {
+    color: #404040;
     border-bottom: 2px solid ${props => props.theme.colors.lightgreen};
     @media (max-width: 768px) {
       font-size: 0.9rem;
+    }
+  }
+  a {
+    text-decoration: none;
+    color: #404040;
+    background: ${props => props.theme.colors.lightgreen};
+    padding: 0.6rem 1.2rem;
+    color: #fff;
+    border-radius: 0.4rem;
+    font-size: 0.9rem;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &.button {
+    position: relative;
+
+    @media (max-width: 768px) {
+      a {
+        position: absolute;
+        right: 0;
+        top: -1.6rem;
+      }
     }
   }
 `;
